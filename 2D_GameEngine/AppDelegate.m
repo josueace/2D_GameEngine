5bca308a1c77d61151e30ac3e5585d00263e3a48
Josue Acevedo <josueacev2@gmail.com>
2024-10-03 16:51:37 -0400

    Initial Commit

---
+++ 2D_GameEngine/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  2D_GameEngine
+//
+//  Created by Josue Acevedo on 10/3/24.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+    return YES;
+}
+
+
+@end
---
+++ 2D_GameEngineTests/_D_GameEngineTests.m
@@ -0,0 +1,36 @@
+//
+//  _D_GameEngineTests.m
+//  2D_GameEngineTests
+//
+//  Created by Josue Acevedo on 10/3/24.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _D_GameEngineTests : XCTestCase
+
+@end
+
+@implementation _D_GameEngineTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
