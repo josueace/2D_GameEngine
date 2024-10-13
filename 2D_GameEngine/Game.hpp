88be59c77d1c32d5350ece4d93e7ba336961acac
Josue Acevedo <josueacev2@gmail.com>
2024-10-12 23:53:02 -0400

    engineloop

---
+++ 2D_GameEngine/Game.hpp
@@ -0,0 +1,32 @@
+//
+//  Game.hpp
+//  2D_GameEngine
+//
+//  Created by Josue Acevedo on 10/3/24.
+//
+
+#ifndef Game_hpp
+#define Game_hpp
+
+#include "SDL2/SDL.h"
+#include <iostream>
+
+class Game {
+    
+public:
+    Game();
+    ~Game();
+    void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+    void handleEvents();
+    void update();
+    void render();
+    void clean();
+    bool running() {return isRunning;}
+private:
+    int cnt = 0;
+    bool isRunning;
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+};
+
+#endif /* Game_hpp */
